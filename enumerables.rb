dde534b1e552d40b4ebc3482c3c92ec10be1d055
codeamac02 <codeamac02@gmail.com>
2015-10-20 10:32:06 -0500

    método de elementos con índice impar

---
+++ enumerables.rb
@@ -0,0 +1,15 @@
+# Dado un array de números imprime únicamente los elementos que contienen un indice impar
+# Utiliza: Enumerable#each_with_index
+def print_odd_indexed_integers(array)
+		#Hash is initialized
+		hash = Hash.new
+		#it evaluates if Array elements has odd index 
+		array.each_with_index { |num, index|
+			hash[num] = index if index != 0 && index %2 != 0 || index == 1
+		}
+		#result, elements with odd index
+		hash.keys
+end
+
+# Driver code...
+p print_odd_indexed_integers([2, 4, 6, 8, 10, 12]) 
